41d6c7b61a466b6f399d237feccfd2b8bacb44d9
jmfrivera <150635986+jmfrivera@users.noreply.github.com>
2026-01-02 22:56:46 +0800

    analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-// 1. Import BrowserRouter
 import { BrowserRouter } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* 2. Wrap App inside BrowserRouter */}
+    {}
     <BrowserRouter>
       <App />
+      <Analytics />
     </BrowserRouter>
   </React.StrictMode>
 );
